55d2b1d41aca7663a05d8c55a025912efa814425
rossescl <rosses@gmail.com>
2020-08-03 14:37:50 -0400

    OK

--- src/android/BarcodeReceiverProindar.java
+++ src/android/BarcodeReceiverProindar.java
@@ -45,27 +45,67 @@ public class BarcodeReceiverProindar extends BroadcastReceiver {
             System.out.println("IntentPlugin::barcode_string::"+strBarcode); 
 
 
-            String command = intent.getStringExtra("COMMAND");
-            String commandidentifier = intent.getStringExtra("COMMAND_IDENTIFIER");
-            String result = intent.getStringExtra("RESULT");
+            String action = intent.getAction();
 
-            Bundle bundle = new Bundle();
+            String strFinalResult = "";
+            String command = intent.getStringExtra("COMMAND");
+            String profileName = intent.getStringExtra("PROFILE_NAME");
             String resultInfo = "";
-            if(intent.hasExtra("RESULT_INFO")){
-                bundle = intent.getBundleExtra("RESULT_INFO");
+
+            if (intent.hasExtra("RESULT_LIST")) { // returns for COMPLETE_RESULT
+                System.out.println("IntentPlugin::RESULT_LIST"); 
+                resultInfo += "ProfileName: " + profileName + "\n";
+                ArrayList<Bundle> result_list = (ArrayList)intent.getSerializableExtra("RESULT_LIST");
+                for (Bundle bundleResult : result_list) {
+
+                    resultInfo +="\n\n";
+
+                    Set<String> keys = bundleResult.keySet();
+                    for (String key : keys) {
+                        String val = bundleResult.getString(key);
+                        if (val == null) {
+
+                            if (bundleResult.getStringArray(key) != null) {
+                                val = "";
+                                for (String s : bundleResult.getStringArray(key)) {
+                                    val += "" + s + "\n";
+                                }
+                            }
+                        }
+
+                        resultInfo += key + ": " + val + "\n";
+                    }
+                }
+            }
+            else if (intent.hasExtra("RESULT_INFO")) { // returns for LAST_RESULT
+                System.out.println("IntentPlugin::RESULT_INFO"); 
+                String result = intent.getStringExtra("RESULT");
+                Bundle bundle = intent.getBundleExtra("RESULT_INFO");
+                resultInfo += "Result: " + result + "\n";
                 Set<String> keys = bundle.keySet();
-                for (String key: keys) {
-                    resultInfo += key + ": "+bundle.getString(key) + "\n";
+                for (String key : keys) {
+                    String val = bundle.getString(key);
+                    if(val == null) {
+
+                        if(bundle.getStringArray(key) != null) {
+                            val="";
+                            for (String s : bundle.getStringArray(key)) {
+                                val += "" + s + "\n";
+                            }
+                        }
+                    }
+
+                    resultInfo += key + ": " + val + "\n";
+                }
+
+            }
+            if (command != null) {
+                if (command.equalsIgnoreCase("com.symbol.datawedge.api.SET_CONFIG")) {
+                    Log.d("TAG", "#IntentApp# \n\nSetConfig status received:\nResultInfo: \n" + resultInfo);
                 }
             }
 
-            String text = "Command: "+command+"\n" +
-                    "Result: " +result+"\n" +
-                    "Result Info: " +resultInfo + "\n" +
-                    "CID:"+commandidentifier;
-
-
-            System.out.println("string.text::"+text); 
+            System.out.println("IntentPlugin::RESULT_INFO::"+resultInfo); 
 
             callbackContext.success(strBarcode);
         }
